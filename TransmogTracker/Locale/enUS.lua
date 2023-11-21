5a6c02f2c8c631c41494559f364ecded06b18bff
telkar-rg <telkar-rg@gmx.net>
2023-11-21 17:29:15 +0100

    enUS localization reviewed

--- TransmogTracker/Locale/enUS.lua
+++ TransmogTracker/Locale/enUS.lua
@@ -4,4 +4,4 @@ local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "enUS", true)
 if not L then return end
 
 L["SHARDS_NAME"] = "Shards of Illusion"
-L["CHAT_MSG_SYSTEM_PATTERN"] = "^Freigeschaltetes Aussehen zur Transmogrifizierung: \124c%x+\124Hitem:(%d+):[:%d]+\124h%[(.-)%]\124h\124r$" -- needs review
+L["CHAT_MSG_SYSTEM_PATTERN"] = "^Unlocked appearance for transmogrification: \124c%x+\124Hitem:(%d+):[:%d]+\124h%[(.-)%]\124h\124r$"
