d86bf42a6186c569062c6f070ec1d0788a47d880
telkar-rg <telkar-rg@gmx.net>
2023-11-22 10:39:53 +0100

    added localization for addon messages

--- TransmogTracker/Locale/deDE.lua
+++ TransmogTracker/Locale/deDE.lua
@@ -5,3 +5,9 @@ if not L then return end
 
 L["SHARDS_NAME"] = "Splitter der Illusion"
 L["CHAT_MSG_SYSTEM_PATTERN"] = "^Freigeschaltetes Aussehen zur Transmogrifizierung: \124c%x+\124Hitem:(%d+):[:%d]+\124h%[(.-)%]\124h\124r$"
+L["MSG_PATTERN_TRACKING"] = "Erfasse %s"
+
+L["NOTIFY_DB_RESET_CMD"] = "Die Addon-Datenbank wurde zurückgesetzt."
+L["NOTIFY_DB_RESET_FIRST_TIME"] = "Die Addon-Datenbank ist leer, weil dieses Addon zum ersten Mal geladen wurde oder weil der WTF-Ordner gelöscht wurde."
+L["NOTIFY_DB_RESET_VERSION_MISMATCH"] = "Die Addon-Datenbank wurde zurückgesetzt, weil die DB-Version nicht mit dieser Version des Addons übereinstimmte."
+L["NOTIFY_DB_EMPTY"] = "Um bereits freigeschaltene Transmog-Aussehen zu erfassen, besuche den |cff66bbffErhabener Illusionist|r NPC und blättere durch alle bekannte Aussehen."
--- TransmogTracker/Locale/enUS.lua
+++ TransmogTracker/Locale/enUS.lua
@@ -5,3 +5,9 @@ if not L then return end
 
 L["SHARDS_NAME"] = "Shards of Illusion"
 L["CHAT_MSG_SYSTEM_PATTERN"] = "^Unlocked appearance for transmogrification: \124c%x+\124Hitem:(%d+):[:%d]+\124h%[(.-)%]\124h\124r$"
+L["MSG_PATTERN_TRACKING"] = "Tracking %s"
+
+L["NOTIFY_DB_RESET_CMD"] = "The addon database has been reset."
+L["NOTIFY_DB_RESET_FIRST_TIME"] = "The addon database is empty because this is the first time this addon has been loaded or because your WTF folder has been deleted."
+L["NOTIFY_DB_RESET_VERSION_MISMATCH"] = "The addon database has been reset because the DB-Version did not match with this version of the addon."
+L["NOTIFY_DB_EMPTY"] = "In order to track previously unlocked Transmog appearances, visit the |cff66bbffSublime Illusionist|r NPC and browse through all known appearances."
