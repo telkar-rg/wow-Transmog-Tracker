8a6dc8482c4955b9d23fd14be4fdc4a2645183b3
telkar-rg <telkar-rg@gmx.net>
2024-02-22 17:49:06 +0100

    removed "AceHook" dependency, added "AceGUI" lib

--- TransmogTracker/TransmogTracker.lua
+++ TransmogTracker/TransmogTracker.lua
@@ -12,7 +12,7 @@
 
 local ADDON_NAME, ADDON_TABLE = ...
 
-local addon = LibStub("AceAddon-3.0"):NewAddon(ADDON_NAME, "AceConsole-3.0", "AceHook-3.0", "AceEvent-3.0", "AceTimer-3.0")
+local addon = LibStub("AceAddon-3.0"):NewAddon(ADDON_NAME, "AceConsole-3.0", "AceEvent-3.0", "AceTimer-3.0")
 _G[ADDON_NAME] = addon
 
 local L = LibStub("AceLocale-3.0"):GetLocale(ADDON_NAME)
